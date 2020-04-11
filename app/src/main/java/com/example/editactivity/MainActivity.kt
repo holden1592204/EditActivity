5d819a8e70a1acb41bc8b895d0dbf5533473f2ab
holden1592204 <warhead.1592204@gmail.com>
2020-04-11 22:22:58 +0900

    fixed activity_edit2

--- app/src/main/java/com/example/editactivity/MainActivity.kt
+++ app/src/main/java/com/example/editactivity/MainActivity.kt
@@ -3,38 +3,45 @@
 package com.example.editactivity
 
 import android.content.Intent
+import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
-
-  override fun onResume() {
-    super.onResume()
-    PreferenceManager.getDefaultSharedPreferences(this).apply {
-      val company: String? = getString("company", "")
-      val postal: String? = getString("postal", "")
-      val address = getString("address", "")
-      val tel = getString("tel", "")
-      val fax = getString("fax", "")
-      val email = getString("email", "")
-      val url = getString("url", "")
-      val position = getString("position", "")
-      val name = getString("name", "")
-
-      companyText.text = company
-      postalText.text = postal
-      addressText.text = address
-      telText.text = ("tel:$tel")
-      faxText.text = ("fax$fax")
-      emailText.text = email
-      urlText.text = url
-      positionText.text = position
-      nameText.text = name
-    }
+  override fun onCreate(savedInstanceState: Bundle?) { //既に保存された情報があればonCreate()メソッドで取得して各項目をEditTextに　編集する
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
   }
+
+     override fun onResume() {
+       super.onResume()
+       PreferenceManager.getDefaultSharedPreferences(this).apply {
+         val company = getString("company", "")
+         val postal = getString("postal", "")
+         val address = getString("address", "")
+         val tel = getString("tel", "")
+         val fax = getString("fax", "")
+         val email = getString("email", "")
+         val url = getString("url", "")
+         val position = getString("position", "")
+         val name = getString("name", "")
+
+         companyText.text = company!!
+         postalText.text = postal
+         addressText.text = address
+         telText.text = ("tel:$tel")
+         faxText.text = ("fax$fax")
+         emailText.text = email
+         urlText.text = url
+         positionText.text = position
+         nameText.text = name
+       }
+     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
       menuInflater.inflate(R.menu.main, menu)
       return true
@@ -51,4 +58,5 @@ class MainActivity : AppCompatActivity() {
       return super.onOptionsItemSelected(item)
     }
   }
+
 
